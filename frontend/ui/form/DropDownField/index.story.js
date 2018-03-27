37c16b78c1da0052df8de06ba55be393bb47043b
looktoroot <looktoroot@gmail.com>
2018-03-27 14:23:56 +0700

    upd DropDownField story

--- frontend/ui/form/DropDownField/index.story.js
+++ frontend/ui/form/DropDownField/index.story.js
@@ -1,41 +1,100 @@
 import React from 'react';
-
 import {storiesOf} from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import { withReadme } from 'storybook-readme';
+import {text, boolean, object, number } from '@storybook/addon-knobs/react';
 
 import DropDownField from './DropDownField';
+import README from './README.md'
+
+const items = [
+    {
+        id: 1,
+        label: 'First',
+    },
+    {
+        id: 2,
+        label: 'Second',
+    },
+    {
+        id: 3,
+        label: 'Third',
+    },
+    {
+        id: 4,
+        label: 'Fourth',
+    },
+];
+
 
 storiesOf('Form', module)
-    .add('DropDownField', () => (
+    .addDecorator(withReadme(README))
+    .add('DropDownField', context => (
         <div>
-            <DropDownField
-                label='Single'
-                searchPlaceholder='Поиск'
-                items={[
-                    {
-                        id: 1,
-                        label: 'First',
-                    },
-                    {
-                        id: 2,
-                        label: 'Second',
-                    },
-                ]}
-            />
-            <DropDownField
-                label='Multiple'
-                searchPlaceholder='Поиск'
-                multiple
-                autoComplete
-                items={[
-                    {
-                        id: 1,
-                        label: 'First',
-                    },
-                    {
-                        id: 2,
-                        label: 'Second',
-                    },
-                ]}
-            />
+            {withInfo()(() => (
+                <DropDownField
+                    label={text('Label', 'Dropdown')}
+                    disabled={boolean('Disabled', DropDownField.defaultProps.disabled)}
+                    required={boolean('Required', DropDownField.defaultProps.required)}
+                    className={text('Class', DropDownField.defaultProps.className)}
+                    showReset={boolean('Show Reset', DropDownField.defaultProps.showReset)}
+                    autoComplete={boolean('Auto Complete', DropDownField.defaultProps.autoComplete)}
+                    // autoCompleteMinLength={number('Auto Complete Min Length')}
+                    // autoCompleteDelay={number('Auto Complete Delay')}
+                    searchPlaceholder={text('Search Placeholder', DropDownField.defaultProps.searchPlaceholder)}
+                    multiple={boolean('Multiple', DropDownField.defaultProps.multiple)}
+                    items={object('Items', items)}
+                />
+            ))(context)}
+
+            <div className='row mb-4'>
+                <div className='col'>
+                    <DropDownField
+                        label='Disabled'
+                        disabled
+                        items={items}
+                    />
+                </div>
+                <div className='col'>
+                    <DropDownField
+                        label='Required'
+                        required
+                        items={items}
+                    />
+                </div>
+                <div className='col'>
+                    <DropDownField
+                        label='Show reset'
+                        showReset
+                        items={items}
+                    />
+                </div>
+            </div>
+
+            <div className='row mb-4'>
+                <div className='col'>
+                    <DropDownField
+                        label='Auto Complete'
+                        autoComplete
+                        items={items}
+
+                    />
+                </div>
+                <div className='col'>
+                    <DropDownField
+                        label='Search Placeholder'
+                        autoComplete
+                        searchPlaceholder='Search..'
+                        items={items}
+                    />
+                </div>
+                <div className='col'>
+                    <DropDownField
+                        label='Multiple'
+                        multiple
+                        items={items}
+                    />
+                </div>
+            </div>
         </div>
     ));
