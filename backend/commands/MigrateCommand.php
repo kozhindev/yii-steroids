ad0af7cd87f91c7002f272d0260546f1fa18b251
Kozhin Vladimir <affka@affka.ru>
2018-06-25 18:21:23 +0700

    fix migrations search

--- backend/commands/MigrateCommand.php
+++ backend/commands/MigrateCommand.php
@@ -2,6 +2,7 @@
 
 namespace steroids\commands;
 
+use yii\base\Module;
 use yii\console\controllers\MigrateController;
 
 class MigrateCommand extends MigrateController
@@ -15,11 +16,28 @@ class MigrateCommand extends MigrateController
         // Set migration namespaces
         foreach (scandir($appPath) as $dirName) {
             $namespace = 'app\\' . $dirName . '\migrations';
-            $this->migrationNamespaces[] = $namespace;
 
-            \Yii::setAlias('@' . $namespace,$appPath . '/' . $dirName . '/migrations');
+            $this->migrationNamespaces[] = $namespace;
+            \Yii::setAlias('@' . $namespace, $appPath . '/' . $dirName . '/migrations');
         }
 
+        $this->scanNamespacesFromModules(\Yii::$app);
+
         return parent::beforeAction($action);
+    }
+
+    protected function scanNamespacesFromModules($module)
+    {
+        foreach ($module->modules as $module) {
+            $info = new \ReflectionClass(is_object($module) ? get_class($module) : $module['class']);
+            $namespace = $info->getNamespaceName() . '\\migrations';
+
+            $this->migrationNamespaces[] = $namespace;
+            \Yii::setAlias('@' . $namespace, dirname($info->getFileName()) . '/migrations');
+
+            if ($module instanceof Module) {
+                $this->scanNamespacesFromModules($module);
+            }
+        }
     }
 }
