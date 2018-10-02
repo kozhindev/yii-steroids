a15e77ab6ac6a335b30ed945cc6a08f7a7384831
Kozhin Vladimir <affka@affka.ru>
2018-10-02 15:01:32 +0700

    sm fixes

--- ui/form/GeetestField/GeetestField.js
+++ ui/form/GeetestField/GeetestField.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
-import {resource, locale} from 'components';
+import {resource, locale, ui} from 'components';
 import fieldHoc from '../fieldHoc';
 
 export default
